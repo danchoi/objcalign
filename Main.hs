0a3ec4682ca084e61d1ff6fc37a70c191e073b0b
Daniel Choi <dhchoi@gmail.com>
2015-11-09 14:20:52 -0500

    initial implementation

---
+++ Main.hs
@@ -0,0 +1,43 @@
+module Main where
+import Data.List (elemIndex)
+
+
+
+
+-- Data.List elemIndex :: Eq a => a -> [a] -> Maybe Int
+
+
+
+alignMethods :: String -> String
+alignMethods s = 
+    let xs              = lines s
+        maxIndent = foldr maxColonIndent 0 xs 
+        xs' = map (reIndent maxIndent) xs
+    in unlines xs'
+
+reIndent :: Int -> String -> String
+reIndent 0 s = s
+reIndent n s | n > (colonIndex s) = addSpaces (n - (colonIndex s)) s
+             | n < (colonIndex s) = removeSpaces ((colonIndex s) - n) s
+reIndent _ s = s
+
+addSpaces n s = (take n $ repeat ' ') ++ s
+
+-- Prelude dropWhile :: (a -> Bool) -> [a] -> [a]
+removeSpaces n s = 
+    let s' = dropWhile (== ' ') s
+    in addSpaces n s'
+
+maxColonIndent :: String -> Int -> Int 
+maxColonIndent s oldMax = max oldMax (colonIndex s) 
+
+colonIndex :: String -> Int
+colonIndex s = maybe 0 id (elemIndex ':' s)
+
+-- Prelude foldr :: (a -> b -> b) -> b -> [a] -> b
+
+-- Data.List foldl' :: (a -> b -> a) -> a -> [b] -> a
+
+main =  do 
+  interact alignMethods
+    
---
+++ README.md
@@ -0,0 +1,10 @@
+# objcalign
+
+
+This simple command line tool aligns Objective-C methods on the colon.
+
+You can use it, e.g., in Vim by highlighting a range of text representing 
+a method, and running `:!objcalign`
+
+The command assumes that the keywords and params are already on separate lines.
+
